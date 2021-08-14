94f2ebc3d6bc64f4a6102b6709ae21b096022809
SuJiaKuan <feabries@gmail.com>
2021-08-14 22:02:29 +0900

    Add a training dataset class for defects removal

--- dataset.py
+++ dataset.py
@@ -12,8 +12,14 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import os
 import glob
+
+import numpy as np
 import torch
+import torchvision
 from torch.utils.data.dataset import Dataset
+
+from utils import get_imagenames
+from utils import open_image
 from utils import open_sequence
 
 
@@ -21,6 +27,90 @@ from utils import open_sequence
 NUMFRXSEQ_VAL = 15
 # pattern for name of validation sequence
 VALSEQPATT = "*"
+
+
+class TrainDataset(Dataset):
+
+    def __init__(
+        self,
+        dataset_root,
+        gray_mode=False,
+        sequence_length=5,
+        crop_size=96,
+    ):
+        self._gray_mode = gray_mode
+        self._crop_size = crop_size
+
+        self._crop = torchvision.transforms.RandomCrop(size=self._crop_size)
+
+        # Collect directories for noisy videos.
+        noisy_dirs = sorted(glob.glob(
+            os.path.join(dataset_root, "before", "*"),
+        ))
+        # Collect directories for the corresponding clean videos.
+        clean_dirs = sorted(glob.glob(
+            os.path.join(dataset_root, "after", "*"),
+        ))
+
+        imagenames_pairs = []
+        keyframe_pad = sequence_length // 2
+        for noisy_dir, clean_dir in zip(noisy_dirs, clean_dirs):
+            # Collect noisy image filenames for a given video.
+            noisy_imagenames = get_imagenames(noisy_dir)
+            # Collect corresponding clean image filenames for a given video.
+            clean_imagenames = get_imagenames(clean_dir)
+
+            assert len(noisy_imagenames) == len(clean_imagenames), \
+                "Number of images are not equal for noisy and clean pair"
+
+            keyframe_start = keyframe_pad
+            keyframe_end = len(noisy_imagenames) - keyframe_pad
+            for keyframe_index in range(keyframe_start, keyframe_end):
+                # For each timestamp of the video, we pick up a pair of noisy
+                # images sequence and it corresponding clean image.
+                noisy_start = keyframe_index - keyframe_pad
+                noisy_end = keyframe_index + keyframe_pad + 1
+                noisy_frames = noisy_imagenames[noisy_start:noisy_end]
+                clean_frame = clean_imagenames[keyframe_index]
+                imagenames_pairs.append((noisy_frames, clean_frame))
+
+        self._imagenames_pairs = imagenames_pairs
+
+    def __getitem__(self, index):
+        imagenames_pair = self._imagenames_pairs[index]
+
+        # Load noisy images sequence.
+        images = [
+            open_image(
+                imagename,
+                gray_mode=self._gray_mode,
+                expand_if_needed=False,
+                expand_axis0=False,
+            )[0]
+            for imagename in imagenames_pair[0]
+        ]
+        # Load corresponding clean image.
+        images.append(open_image(
+            imagenames_pair[1],
+            gray_mode=self._gray_mode,
+            expand_if_needed=False,
+            expand_axis0=False,
+        )[0])
+        # Convert the images into Tensor.
+        images = torch.from_numpy(np.array(images))
+
+        # Apply random crop on both noisy images sequence and clean image.
+        patches = self._crop(images)
+        # Get cropped noisy patches.
+        noisy_patches = patches[:-1]
+        noisy_patches = noisy_patches.view((-1, *noisy_patches.shape[2:]))
+        # Get cropped clean patches.
+        clean_patch = patches[-1]
+
+        return noisy_patches, clean_patch
+
+    def __len__(self):
+        return len(self._imagenames_pairs)
 
 
 class ValDataset(Dataset):
